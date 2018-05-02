e8e51c39da76c01855245c1c3ac7e7186a8fb104
Damien Alves <alves.d24@laposte.net>
2018-05-02 16:13:05 +0300

    data recup in realTime

--- sockets_connexion_fxcm.js
+++ sockets_connexion_fxcm.js
@@ -26,8 +26,9 @@ let getConnexionFXCM = (token) => {
     console.log('Socket.IO session has been opened: ', socket.id);
     requestHeaders.Authorization = 'Bearer ' + socket.id + token;
     getAccountId();
-/*     suscribePrices(socket);
- */  });
+    suscribePrices(socket);
+
+  });
   socket.on('connect_error', (error) => {
     console.log('Socket.IO session connect error: ', error);
   });
@@ -53,6 +54,57 @@ let getAccountId = () => {
     console.log(error)
   })
 };
+
+let suscribePrices = (socket) => {
+  let resource = '/subscribe';
+  let pairs = {
+    "pairs": ["EUR/USD", "AUD/USD"]
+  }
+  let postData = querystring.stringify(pairs);
+
+  let option = {
+    host: config.configFxcm.host,
+    port: 443,
+    method: 'POST',
+    path: resource,
+    headers: requestHeaders
+  }
+
+  let req = https.request(option, (res) => {
+    let result = '';
+    res.on('data', function (chunk) {
+      result += chunk;
+    });
+    res.on('end', function () {
+      let jsonData = JSON.parse(result);
+      for (var i in jsonData.pairs) {
+        socket.on(jsonData.pairs[i].Symbol, priceUpdate);
+      }
+    });
+    res.on('error', function (err) {
+      console.log('Error : ', err);
+    })
+  }).on('error', function (err) {
+    console.log('Req error : ', err);
+  });
+  req.write(postData);
+  req.end();
+}
+
+var priceUpdate = (update) => {
+  try {
+    var jsonData = JSON.parse(update);
+
+    jsonData.Rates = jsonData.Rates.map(function (element) {
+      return element.toFixed(5);
+    });
+    console.log(`@${jsonData.Updated} Price update of [${jsonData.Symbol}]: ${jsonData.Rates}`);
+  } catch (e) {
+    console.log('price update JSON parse error: ', e);
+    return;
+  }
+}
+
 
 module.exports = {
   getConnexionFXCM,
